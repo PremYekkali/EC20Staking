f2cd3cc7d2f25eb86ac632f5ab54cb7d6119a10b
PremYekkali <yekkaliprem@gmail.com>
2026-01-20 15:22:03 +0530

    Improve contract documentation using NatSpec comments

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+/// @title ERC20 Staking Contract
+/// @author Prem
+/// @notice Allows users to stake ERC20 tokens and earn time based rewards
+/// @dev Rewards are calculated based on staking duration
 contract Staking is ReentrancyGuard {
     IERC20 public immutable token;
 
@@ -18,11 +22,17 @@ contract Staking is ReentrancyGuard {
     event Redeemed(address indexed user, uint256 amount);
     event InterestClaimed(address indexed user, uint256 amount);
 
+    /// @notice Initializes the staking contract with the ERC20 token
+    /// @param tokenAddress Address of the ERC20 token to be staked
     constructor(address tokenAddress) {
         require(tokenAddress != address(0), "Invalid token");
         token = IERC20(tokenAddress);
     }
 
+    /// @notice Stake ERC20 tokens to earn rewards
+    /// @param amount Amount of tokens to stake
+    /// @dev If the user already has an active stake, the existing stake and any
+    /// accrued rewards are fully settled and a new staking position is created
     function stake(uint256 amount) external nonReentrant {
         require(amount > 0, "Invalid amount");
 
@@ -44,6 +54,9 @@ contract Staking is ReentrancyGuard {
         emit Staked(msg.sender, amount);
     }
 
+    /// @notice Redeem staked tokens and receive any accrued rewards
+    /// @param amount Amount of staked tokens to redeem
+    /// @dev Redeeming resets the staking timer for the remaining balance
     function redeem(uint256 amount) external nonReentrant {
         require(amount > 0, "Invalid amount");
 
@@ -69,6 +82,8 @@ contract Staking is ReentrancyGuard {
         emit Redeemed(msg.sender, amount);
     }
 
+    /// @notice Claim accrued staking rewards
+    /// @dev Reverts if no rewards are available to claim
     function claimInterest() external nonReentrant {
         uint256 interest = _calculateInterest(msg.sender);
         require(interest > 0, "No interest");
